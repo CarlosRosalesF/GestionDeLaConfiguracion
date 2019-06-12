d06d05537dcd7832d231c8c958612ea054897478
Carlos Rosales <carlos.rosales0615@gmail.com>
2019-06-11 21:33:33 -0500

    first commit

---
+++ src/main/java/com/usb/app/App.java
@@ -0,0 +1,13 @@
+package com.usb.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
